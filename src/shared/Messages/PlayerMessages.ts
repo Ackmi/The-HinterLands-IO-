d3529d069fe17d11a5530d83b49f9896c84783f2
Chris Moeller <chris.a.moeller@gmail.com>
2018-06-24 13:40:12 -0700

    basic networking messages

---
+++ src/shared/Messages/PlayerMessages.ts
@@ -0,0 +1,14 @@
+
+class PlayerMessages
+{
+    static Position = class
+    {
+        x:number;
+        y:number;
+        constructor(x:number, y:number)
+        {
+            this.x=x;
+            this.y=y;
+        }
+    }
+}
